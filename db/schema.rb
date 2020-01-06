02299161228e4b61ca71111608c8d13c7ed82b2a
Philippe CHIRIE <p.chirie@laposte.net>
2020-01-06 17:57:58 +0100

    ModalUpdate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_155120) do
+ActiveRecord::Schema.define(version: 2020_01_02_155851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "sku"
+    t.string "name"
+    t.bigint "category_id"
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_01_02_155120) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Product.destroy_all
+Category.destroy_all
+
+puts 'Creating categories...'
+serv = Category.new(name: 'Service')
+serv.save
+arti = Category.new(name: 'Article')
+arti.save
+tran = Category.new(name: 'Transport')
+tran.save
+
+puts 'Creating products...'
+
+50.times do
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    category_id: 1
+  )
+end
+puts 'Finished!'
+
+50.times do
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    category_id: 2
+  )
+end
+puts 'Finished!'
+
+50.times do
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    category_id: 3
+  )
+end
+puts 'Finished!'
